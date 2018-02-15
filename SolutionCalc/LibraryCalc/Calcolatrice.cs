c54ca2e309684fc527438143f4d78d60b71a1ffc
studenti <studenti@LAB2-PC-08>
2018-02-15 08:50:38 +0100

    Media massimo e minimo

---
+++ SolutionCalc/LibraryCalc/Calcolatrice.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryCalc
+{
+    class Calcolatrice
+    {
+        public static double Massimo(double a, double b)
+        {
+            double max = 0;
+            if (a > b)
+            {
+                max = a;
+            }
+            else
+            {
+                max = b;
+            }
+            return max;
+        }
+        public static double Minimo(double a, double b)
+        {
+            double min = 0;
+            if (a > b)
+            {
+                min = b;
+            }
+            else
+            {
+                min = a;
+            }
+            return min;
+        }
+        public static double Media(double a, double b)
+        {
+            double ris = 0;
+            ris = (a + b) / 2;
+            return ris;
+        }
+
+    }
+}
